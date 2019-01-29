12446b5f826baf810fbc373d18adc88cf5cd4c0e
wcadena2018 <william.cadena@gmail.com>
2019-01-29 17:24:48 -0500

    hola

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Welcome Willian it is your new job!")
+                                    .addInitParam("message", "Welcome Equipo de Arquitectos it is your new job!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
